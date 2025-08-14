3ff60ae9599c9b6239f89ba84b4cccf9a3edcc70
Fahrettin ALTAY <fahrettinalty@gmail.com>
2025-08-14 21:53:32 +0300

    User and box conditions have been updated.

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ window.addEventListener('DOMContentLoaded', () => {
     let currentRowIndex = 0 // Which line are we on
     let currentTileInRow = 0 // Box index in line
 
-    function nextRow(key){
-        if (currentTileInRow === 5 && key !== 'Backspace') { // if the line is complete
-            currentRowIndex++
-            currentTileInRow = 0
-        }
-    }
-
     // It works when any key is pressed on the keyboard, the information about the pressed key is in event.key
     window.addEventListener('keydown', (event) => {
         const key = event.key;
@@ -19,13 +12,18 @@ window.addEventListener('DOMContentLoaded', () => {
         if (key === 'Backspace' && currentTileInRow > 0) {
                 currentTileInRow--
                 currentRow[currentTileInRow].textContent = ''
-
-        } else if (key.length === 1 && key.match(/[a-z]/i)){
+        }
+        
+        
+        if (key === 'Enter' && currentTileInRow === 5 ) {
+            currentRowIndex++
+            currentTileInRow = 0
+        }
+            
+         if (key.length === 1 && key.match(/[a-z]/i)){
             if (currentTileInRow < currentRow.length) {
-                console.log(currentTileInRow)
                 currentRow[currentTileInRow].textContent = key.toUpperCase()
                 currentTileInRow++
-                nextRow(key)
             }
                 
         }
